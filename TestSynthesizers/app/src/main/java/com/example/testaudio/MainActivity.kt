cea8bb2a1ad648fa80cf4e59ad54122a97308833
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2021-10-06 16:38:04 -0600

    working version 2

--- TestSynthesizers/app/src/main/java/com/example/testaudio/MainActivity.kt
+++ TestSynthesizers/app/src/main/java/com/example/testaudio/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.example.testaudio
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.karlotoy.perfectune.instance.PerfectTune
 import android.os.CountDownTimer
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.karlotoy.perfectune.instance.PerfectTune
 import android.view.View
-import android.widget.Toast
 import java.util.*
 import kotlin.random.Random
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
     // How many notes
     var melodyLength = 7
     // 1. Create an empty list to later store notes
-    var listOfPlayers = mutableListOf<Note>()
+    var notes = mutableListOf<Note>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,36 +30,70 @@ class MainActivity : AppCompatActivity() {
             var randomFrequency = Random.nextDouble(220.0000,880.0000)
             var randomDuration = Random.nextInt(500, 2000)
             var note = Note()
-            note.library = PerfectTune()
+            note.toneObject = PerfectTune()
             note.frequency = randomFrequency
             note.duration = randomDuration
-            listOfPlayers.add(note)
+            notes.add(note)
         }
-        return listOfPlayers
+        return notes
     }
 
-    var perfectTuneTest = PerfectTune()
+    /**
+     * PLAY button
+     */
+//    fun playMelody(view: View) {
+//        if (view.id == R.id.button_sound_play) {
+//            for (n2 in listOfPlayers) {
+//                Log.i("note", "Step into Loop - note: ${n2.frequency}")
+//                n2.assignFrequency()
+//                object : CountDownTimer((n2.duration.toLong()), 1000) {
+//                    override fun onTick(millisUntilFinished: Long) {
+//                        n2.playFreq()
+//                        Log.i("note", "onTick method: Playing note ${n2.frequency} for ${n2.duration} milliseconds")
+//                    }
+//                    override fun onFinish() {
+//                        n2.toneObject.stopTune()
+//                        Log.i("note", "onFinish method: complete")
+//                    }
+//                }.start()
+//                Log.i("note", "start CountDownTimer")
+//            }
+//            Log.i("note", "Finished loop")
+//        }
+//    }
+
     fun playMelody(view: View) {
+        var index = 0
         if (view.id == R.id.button_sound_play) {
-            for (n2 in listOfPlayers) {
-                Log.i("note", "Step into Loop - note: ${n2.frequency}")
-                object : CountDownTimer((n2.duration.toLong()), 1000) {
-                    override fun onTick(millisUntilFinished: Long) {
-                        n2.library.tuneFreq = n2.frequency
-                        n2.library.playTune()
-                        Log.i("note", "onTick method: Playing note ${n2.frequency} for ${n2.duration} milliseconds")
-                    }
-                    override fun onFinish() {
-                        n2.library.stopTune()
-                        Log.i("note", "onFinish method: complete")
-                    }
-                }.start()
-                Log.i("note", "start CountDownTimer")
-            }
-            Log.i("note", "Finished loop")
+            object: CountDownTimer((melodyLength*1000).toLong(), 1000) {
+                override fun onTick(millisUntilFinished: Long) {
+                    Log.i("note", "note frequency: ${notes[index].frequency}")
+                    notes[index].assignFrequency()
+                    notes[index].playFreq()
+                    startCoolDown(notes[index],1)
+                    index += 1
+                }
+                override fun onFinish() {
+                    return
+                }
+            }.start()
         }
     }
 
+    fun startCoolDown(note: Note, seconds: Int) {
+        object: CountDownTimer(seconds*1000.toLong(), seconds*1000.toLong()) {
+            override fun onTick(millisUntilFinished: Long) {
+
+            }
+            override fun onFinish() {
+                note.toneObject.stopTune()
+            }
+        }.start()
+    }
+
+    /**
+     * STOP button
+     */
 //    fun stopTune(view: View) {
 //        if (view.id == R.id.button_sound_stop) {
 //            //stops the tune
--- TestSynthesizers/app/src/main/java/com/example/testaudio/Note.kt
+++ TestSynthesizers/app/src/main/java/com/example/testaudio/Note.kt
@@ -1,16 +1,33 @@
 package com.example.testaudio
 
 import com.karlotoy.perfectune.instance.PerfectTune
+import kotlin.random.Random
 
 class Note() {
     var frequency: Double
-    var library: PerfectTune
+    var toneObject: PerfectTune
     var duration: Int
 
     init {
         frequency = 0.0
-        library = PerfectTune()
+        toneObject = PerfectTune()
         duration = 1000
+    }
+
+    fun assignDuration() {
+        var randomDuration = Random.nextInt(500, 2000)
+        duration = randomDuration
+    }
+
+    fun assignFrequency() {
+//        var randomFrequency = Random.nextDouble(220.0000,880.0000)
+//        frequency = randomFrequency
+        toneObject.tuneFreq = frequency
+    }
+
+    fun playFreq() {
+        toneObject.tuneAmplitude = 10000
+        toneObject.playTune()
     }
 
 }
